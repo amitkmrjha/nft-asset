1514c7b2e8fa84459ad5d00c4a798a825bc81866
amitkmrjha <amitjha.manipal@gmail.com>
2024-09-15 18:23:24 +0200

    empty project

--- nft-asset-svc/src/main/scala/com/aw/nft/asset/NFTAssetMain.scala
+++ nft-asset-svc/src/main/scala/com/aw/nft/asset/NFTAssetMain.scala
@@ -3,13 +3,13 @@ package com.aw.nft.asset
 import com.typesafe.config.ConfigFactory
 import org.apache.pekko.actor.typed.ActorSystem
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
-import org.apache.pekko.http.scaladsl.Http
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.util.control.NonFatal
 
 object NFTAssetMain:
+
   val logger = LoggerFactory.getLogger(NFTAssetMain.getClass)
 
   lazy val config =
@@ -33,18 +33,4 @@ object NFTAssetMain:
 
   def init(
       system: ActorSystem[?]
-  ): Unit =
-    startGrpc(system)
-
-  private def startGrpc(
-      system: ActorSystem[?]
-  ): Future[Http.ServerBinding] =
-    given ActorSystem[?]   = system
-    given ExecutionContext = system.executionContext
-
-    val grpcInterface                       = system.settings.config.getString("nft-asset-svc.grpc.interface")
-    val grpcPort                            = system.settings.config.getInt("nft-asset-svc.grpc.port")
-    val serviceImpl                         = new NFTAssetServiceImpl
-    val binding: Future[Http.ServerBinding] = NFTAssetServer.start(grpcInterface, grpcPort, serviceImpl)
-    system.log.info(s"NFTAsset gRPC server running at $grpcInterface:$grpcPort")
-    binding
+  ): Unit = {}
--- nft-asset-svc/src/test/scala/com/aw/nft/asset/svc/NFTAssetGrpcSpec.scala
+++ nft-asset-svc/src/test/scala/com/aw/nft/asset/svc/NFTAssetGrpcSpec.scala
@@ -1,20 +1,14 @@
 package com.aw.nft.asset.svc
 
-import com.aw.nft.asset.{NFTAssetServer, NFTAssetServiceImpl}
-import com.aw.nft.grpc.{GetHealthResponse, NFTAssetServiceClient}
-import org.scalatest.wordspec.AsyncWordSpec
-import com.google.protobuf.empty.Empty
 import com.typesafe.config.ConfigFactory
 import org.apache.pekko.actor.testkit.typed.scaladsl.ActorTestKit
 import org.apache.pekko.actor.typed.ActorSystem
-import org.apache.pekko.grpc.GrpcClientSettings
-import org.apache.pekko.http.scaladsl.Http
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.shouldBe
+import org.scalatest.wordspec.AsyncWordSpec
 
-import java.util.UUID
 import scala.concurrent.Future
 import scala.concurrent.duration.*
 
@@ -33,32 +27,19 @@ class NFTAssetGrpcSpec extends AsyncWordSpec with Matchers with BeforeAndAfterAl
 
   lazy val grpcInterface = config.getString("nft-asset-svc.grpc.interface")
   lazy val grpcPort      = config.getInt("nft-asset-svc.grpc.port")
-  lazy val serviceImpl   = new NFTAssetServiceImpl()
 
   val testKit: ActorTestKit = ActorTestKit(config)
 
   given typedSystem: ActorSystem[?] = testKit.system
 
-  def clientSettings: GrpcClientSettings = GrpcClientSettings
-    .fromConfig(clientName = "nft-asset-grpc-test-client")(typedSystem)
-
-  val assetGrpcClient: NFTAssetServiceClient =
-    NFTAssetServiceClient.apply(clientSettings)
-
   override def beforeAll(): Unit =
-    val service: Future[Http.ServerBinding] = NFTAssetServer.start(grpcInterface, grpcPort, serviceImpl)
+    super.beforeAll()
 
   override def afterAll(): Unit =
     testKit.shutdownTestKit()
 
   "NFTAsset Grpc service" should {
-    "check health of gRPC server" in {
-      val request: Empty                      = Empty.defaultInstance
-      val response: Future[GetHealthResponse] =
-        assetGrpcClient
-          .getHealth()
-          .addHeader("x-correlation-id", s"${UUID.randomUUID().toString}")
-          .invoke(request)
-      response.futureValue.message shouldBe "NFTAsset gRPC is healthy!"
+    "return dummy success " in {
+      true shouldBe true
     }
   }
