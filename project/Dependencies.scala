24f3450b9165f7640ef2350d0bfafae803f528be
Amit Kumar <amitjha.manipal@gmail.com>
2025-10-06 11:11:49 +0200

    bump to latest pekko

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,17 +22,18 @@ object Dependencies {
   val pekkoClusterShardingTyped       = "org.apache.pekko"  %% "pekko-cluster-sharding-typed"   % Version.pekko
   val pekkoPersistence                = ("org.apache.pekko" %% "pekko-persistence-typed"        % Version.pekko)
   val pekkoPersistenceTestKit         = "org.apache.pekko"  %% "pekko-persistence-testkit"      % Version.pekko % Test
-  val pekkoPersistenceJdbc            = "org.apache.pekko"  %% "pekko-persistence-jdbc"         % Version.pekko
+  val pekkoPersistenceJdbc            = "org.apache.pekko"  %% "pekko-persistence-jdbc"         % Version.pekkoPersistenceJdbc
   val pekkoPersistenceQuery           = "org.apache.pekko"  %% "pekko-persistence-query"        % Version.pekko
   val pekkoDiscovery                  = "org.apache.pekko"  %% "pekko-discovery"                % Version.pekko
-  val pekkoDiscoveryKubernetes        = "org.apache.pekko"  %% "pekko-discovery-kubernetes-api" % Version.pekko
-  val pekkoManagement                 = "org.apache.pekko"  %% "pekko-management"               % Version.pekko
+  val pekkoDiscoveryKubernetes        = "org.apache.pekko"  %% "pekko-discovery-kubernetes-api" % Version.pekkoManagement
+  val pekkoManagement                 = "org.apache.pekko"  %% "pekko-management"               % Version.pekkoManagement
   val pekkoManagementClusterBootstrap =
-    "org.apache.pekko" %% "pekko-management-cluster-bootstrap" % Version.pekko
-  val pekkoManagementHttp = "org.apache.pekko" %% "pekko-management-cluster-http" % Version.pekko
+    "org.apache.pekko" %% "pekko-management-cluster-bootstrap" % Version.pekkoManagement
+  val pekkoManagementHttp = "org.apache.pekko" %% "pekko-management-cluster-http" % Version.pekkoManagement
   val pekkoHttp           = "org.apache.pekko" %% "pekko-http"                    % Version.pekkoHttp
   val pekkoHttpSprayJson  = "org.apache.pekko" %% "pekko-http-spray-json"         % Version.pekkoHttp
   val pekkoStream         = "org.apache.pekko" %% "pekko-stream"                  % Version.pekko
+  val pekkoStreamsTestKit = "org.apache.pekko" %% "pekko-stream-testkit"          % Version.pekko % "test"
   val pekkoJackson        = "org.apache.pekko" %% "pekko-serialization-jackson"   % Version.pekko
 
   val alpekkoKafka        = ("org.apache.pekko" %% "pekko-connectors-kafka" % Version.pekkoKafka)
